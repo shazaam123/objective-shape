2c9ab689b18b7964d7cc8016f525ed058784d2c9
jobet casquejo <165629120+shazaam123@users.noreply.github.com>
2024-05-14 14:28:05 +0000

    feat: Add BaseModel with created_at and updated_at fields, add Enrollment model with students ForeignKey and import serializers

--- school/enrollment/models.py
+++ school/enrollment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from student.models import Student
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Enrollment(BaseModel):
+    students = models.ForeignKey(Student, on_delete=models.CASCADE)
